c7b21c0ea77faa712bb2113455beffdce3960525
SoniaV <sonia.vidalc1@gmail.com>
2025-03-03 23:32:44 -0300

    Fix CORS

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,14 +4,13 @@ export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whitelist = [process.env.FRONTEND_URL, "http://localhost:5173"];
     if (process.argv[2] === "--api") {
-      whitelist.push(undefined);
+      whitelist.push(undefined, null);
     }
 
-    if (whitelist.includes(origin)) {
+    if (!origin || whitelist.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Error de CORS"));
     }
   },
-  credentials: true,
 };
