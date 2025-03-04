04ece13c3cdf3d7701246a23f56237238f36b2a8
SoniaV <sonia.vidalc1@gmail.com>
2025-03-04 11:24:42 -0300

    Ruta raíz añadida para evitar error 404

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
 import authRoutes from "./routes/authRoutes";
+import type { Request, Response, NextFunction } from "express";
 
 dotenv.config();
 connectDB();
@@ -18,5 +19,19 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
+
+app.get("/", (res: Response) => {
+  res.send("API funcionando correctamente 🚀");
+});
+
+// Manejo de rutas no encontradas
+app.use((res: Response) => {
+  res.status(404).json({ msg: "Ruta no encontrada" });
+});
+
+app.use((err, res: Response) => {
+  console.error(err);
+  res.status(500).json({ msg: "Error del servidor" });
+});
 
 export default app;
